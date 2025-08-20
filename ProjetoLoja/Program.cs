42d179170454722c971d1687677186efa2ced5b2
Juliana <ju.s.oliveira23@gmail.com>
2025-08-20 08:43:59 -0300

    criando as sessões

--- ProjetoLoja/Program.cs
+++ ProjetoLoja/Program.cs
@@ -5,6 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+// 1. REGISTRAR OS SERVIÇOS DE SESSÃO
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Tempo de inatividade da sessão (pode ajustar)
+    options.Cookie.HttpOnly = true; // Impede que o cookie seja acessado por scripts do lado do cliente
+    options.Cookie.IsEssential = true; // Torna o cookie de sessão essencial para a funcionalidade do aplicativo
+});
+
+// REGISTRAR A CONNECTION STRING COMO UM SERVIÇO STRING AQUI
+builder.Services.AddSingleton<string>(builder.Configuration.GetConnectionString("DefaultConnection")!);
+
 // Registrar Repositórios
 builder.Services.AddScoped<ProdutoRepositorio>();
 var app = builder.Build();
