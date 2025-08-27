bde579f90349077223133faa01cd24c3c4fc2b51
Juliana <ju.s.oliveira23@gmail.com>
2025-08-27 08:19:50 -0300

    Criando um método e a conexão com o banco de dados

--- ProjetoLoja/Repositorio/ProdutoRepositorio.cs
+++ ProjetoLoja/Repositorio/ProdutoRepositorio.cs
@@ -1,6 +1,25 @@
-﻿namespace ProjetoLoja.Repositorio
+﻿using MySql.Data.MySqlClient;
+using ProjetoLoja.Models;
+using Dapper;
+
+namespace ProjetoLoja.Repositorio
 {
+
     public class ProdutoRepositorio
     {
+        //verificando a string de conexão do appsettings.json
+        private readonly string _connectionString;
+
+        public ProdutoRepositorio(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        //Criando um método e fazendo a sincronização com o banco de dados
+        public async Task<IEnumerable<Produto>> TodosProdutos()
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            var sql = "SELECT Id, Nome, Descricao, Preco, ImageUrl, Estoque FROM Produtos";
+            return await connection.QueryAsync<Produto>(sql);
+        }
     }
 }
