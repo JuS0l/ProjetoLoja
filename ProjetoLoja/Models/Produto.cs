2edfece241a2b86dadc50aad9a0911fedd3f3493
Juliana <ju.s.oliveira23@gmail.com>
2025-09-03 08:36:11 -0300

    Criando os Produtos

--- ProjetoLoja/Models/Produto.cs
+++ ProjetoLoja/Models/Produto.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
-namespace LojaProdutosAsp.Models
+namespace ProjetoLoja.Models
 {
     public class Produto
     {
